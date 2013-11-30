70984759b32815cd270d258d155a3cddc759f358
Tim Green <timgreen1@outlook.com>
2013-11-30 22:18:45 +0000

    Add static pages, partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Code::Application.routes.draw do
+
+  root 'pages#home'
+
+  match '/help',    to: 'pages#help',    via: 'get'
+  match '/about',   to: 'pages#about',   via: 'get'
+  match '/contact', to: 'pages#contact', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ spec/requests/pages_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe "Pages" do
+  
+  subject { page }
+
+	describe "Home page" do
+		before { visit root_path }
+
+		it { should have_content('Tim Twitter') }
+		it { should have_title(full_title('')) }
+		it { should_not have_title('| Home') }
+	end
+	describe "Help page" do
+		before { visit help_path }
+
+		it { should have_content('Help') }
+		it { should have_title(full_title('Help')) }
+	end
+
+	describe "About page" do
+		before { visit about_path }
+
+		it { should have_content('About') }
+		it { should have_title(full_title('About')) }
+	end
+
+	describe "Contact page" do
+		before { visit contact_path }
+
+		it { should have_content('Contact') }
+		it { should have_title(full_title('Contact')) }
+	end
+end

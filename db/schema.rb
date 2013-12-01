422d746b014ede202208425997915c2c34c85230
Tim Green <timgreen1@outlook.com>
2013-12-01 00:48:15 +0000

    Add name and location to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131130233446) do
+ActiveRecord::Schema.define(version: 20131201004717) do
 
   create_table "users", force: true do |t|
     t.string   "username"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20131130233446) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "name"
+    t.string   "location"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

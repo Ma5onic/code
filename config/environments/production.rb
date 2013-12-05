b0da06ae98d11d3f68305276119d9ac79377a493
Tim Green <timgreen1@outlook.com>
2013-12-05 22:09:46 +0000

    Add index.js to production precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Code::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif index.js)
+  config.assets.precompile += %w( index.js )
 end
